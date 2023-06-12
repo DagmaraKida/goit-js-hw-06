9f1e942ee735a0b348a1550441c38ba3ad2ea8db
Dagmara Kida <dagmarakida@gmail.com>
2023-06-12 22:37:48 +0100

    js task 02

--- js/task-01.js
+++ js/task-01.js
@@ -1,3 +1,9 @@
+/* Write a script that:
+
+Will count and display in the console the number of categories in ul#categories, that is, li.item elements.
+For each li.item element in the ul#categories list, will find and display in the console the element header text 
+(<h2> tag) and the number of elements in the category (all nested <li>). */
+
 const categoryList = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoryList.length}`);
 
@@ -5,4 +11,3 @@ categoryList.forEach((category) => {
   console.log(`Category: ${category.firstElementChild.textContent}`);
   console.log(`Elements: ${category.querySelectorAll("li").length}`);
 });
-
--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,18 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+/* Write a script that, for each element in the ingredients array:
+
+Will create a separate <li> element. Be sure to use the document.createElement() method.
+Will add the ingredient name as its text content.
+Will add the item class to the element.
+Then will insert all <li> to the ul#ingredients list in a single operation.
+ */
+
+const ingredients = ["Potatoes", "Mushrooms", "Garlic", "Tomatos", "Herbs", "Condiments"];
+
+const ingredientsList = document.querySelector("#ingredients");
+
+ingredients.forEach((ingredient) => {
+  const listItem = document.createElement("li");
+  listItem.textContent = ingredient;
+  listItem.classList.add("item");
+  ingredientsList.append(listItem);
+});
