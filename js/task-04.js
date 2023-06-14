2cef660f74ba097ca6ca3edea980bca426421b6d
Dagmara Kida <dagmarakida@gmail.com>
2023-06-14 22:32:43 +0100

    js task 04

--- js/task-04.js
+++ js/task-04.js
@@ -1,3 +1,15 @@
+/* The counter consists of a span and buttons, which, when clicked, should increase and decrease its value by one.
+
+<div id="counter">
+  <button type="button" data-action="decrement">-1</button>
+  <span id="value">0</span>
+  <button type="button" data-action="increment">+1</button>
+</div>
+
+Create a variable, counterValue, that will store the current counter value and initialize it with 0.
+Add listeners for clicks on the buttons; inside them, increase or decrease the value of the counter.
+Update the interface with the new value of the variable counterValue. */
+
 const counter = document.querySelector("#value");
 const decrementButton = document.querySelector('[data-action="decrement"]');
 const incrementButton = document.querySelector('[data-action="increment"]');
