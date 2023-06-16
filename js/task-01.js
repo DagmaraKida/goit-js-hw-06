5aaa3b70a98fd3a3a9e137c7ec3867503f8a609f
Dagmara Kida <dagmarakida@gmail.com>
2023-06-16 13:55:25 +0100

    js task 01 corrected

--- js/task-01.js
+++ js/task-01.js
@@ -8,6 +8,6 @@ const categoryList = document.querySelectorAll(".item");
 console.log(`Number of categories: ${categoryList.length}`);
 
 categoryList.forEach((category) => {
-  console.log(`Category: ${category.firstElementChild.textContent}`);
+  console.log(`Category: ${category.querySelector("h2").textContent}`);
   console.log(`Elements: ${category.querySelectorAll("li").length}`);
 });
